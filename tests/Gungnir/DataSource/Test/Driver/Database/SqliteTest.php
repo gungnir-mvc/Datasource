b43205ca837b40abacdecce20d0a55b3e3c8881f
Conny Karlsson <conny.karlsson@ticketmaster.se>
2017-10-16 09:59:16 +0200

    Adding some unit tests

---
+++ tests/Gungnir/DataSource/Test/Driver/Database/SqliteTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Gungnir\DataSource\Test\Driver\Database;
+
+use PHPUnit\Framework\TestCase;
+
+class SqliteTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
